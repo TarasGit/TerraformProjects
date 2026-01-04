5b3217854e2890b2bd42e18e68d69cb802d91620
Taras Iks <taras@MacBook-Air-von-Taras.fritz.box>
2026-01-04 18:56:07 +0100

    Add Azure Virtual Network

---
+++ AzureVM/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.97.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "vm-rg" {
+  name     = "ubuntu-vm-resources"
+  location = "westeurope"
+  tags = {
+    env = "dev"
+  }
+}
+
+resource "azurerm_virtual_network" "ubuntu-vm-vn" {
+  name                = "ubuntu-vm-network"
+  resource_group_name = azurerm_resource_group.vm-rg.location
+  location            = azurerm_resource_group.vm-rg.location
+  address_space       = ["10.123.0.0/16"]
+  tags = {
+    env = "dev"
+  }
+}
+
+
